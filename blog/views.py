c90d4309ad85d97efac2e60310e98da2e8ab9037
Aayush kapil <aayushkapil100@gmail.com>
2023-11-04 16:01:50 +0530

    f

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render , HttpResponse
 # Create your views here.
 def home(index):
     return HttpResponse("this is home ")
+def bg(index):
+    return HttpResponse("yes how may help you ")
