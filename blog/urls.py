c027c8aaaf5bfcc58fad18fc0bd8cd8f35fbfdea
Aayush kapil <aayushkapil100@gmail.com>
2023-11-04 16:17:28 +0530

    h

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path , include
 from . import views
 
 urlpatterns = [
-     path('' ,views.home , name = 'home' ),
-     path('blog' , views.bg, name = 'bg'),
+     path('' ,views.home , name ='home' ),
+    
 ]
--- home/views.py
+++ home/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render , HttpResponse
 # Create your views here.
 def home(index):
     return HttpResponse("this is home ")
-
+def contact(index):
+    return HttpResponse("this is contact ")
+def about(index):
+    return HttpResponse("this is about page ")
