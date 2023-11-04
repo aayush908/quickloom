7275ff5c116a874be4f8a940d072afdca175eb34
Aayush kapil <aayushkapil100@gmail.com>
2023-11-04 23:06:12 +0530

    f

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class contact(models.Model):
+    sno = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=13)
+    content = models.TextField()
+    timeStamp=models.DateTimeField(auto_now_add=True, blank=True)
