f535ad7a3a505d778aa9cd254b26b6ae2fa61ecd
Aayush kapil <aayushkapil100@gmail.com>
2023-11-04 23:29:49 +0530

    g

--- home/models.py
+++ home/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class contact(models.Model):
+class Contact(models.Model):
     sno = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=100)
--- home/views.py
+++ home/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render , HttpResponse
+from django.shortcuts import render, HttpResponse
+from .models import Contact
 
 # Create your views here.
 def home(request):
-    return render(request , 'home/home.html')
+    return render(request, 'home/home.html')
+
 def contact(request):
-    return render(request , 'home/contact.html')
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        content = request.POST['content']
+
+        contact_entry = Contact(name=name, email=email, phone=phone, content=content)
+        contact_entry.save()
+
+    return render(request, 'home/contact.html')
+
 def about(request):
-    return render(request , 'home/about.html')
+    return render(request, 'home/about.html')
