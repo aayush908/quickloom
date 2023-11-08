69214b3d5e9de70563caf8c427557d64d00cb9c2
Aayush kapil <aayushkapil100@gmail.com>
2023-11-08 21:46:35 +0530

    login

--- home/urls.py
+++ home/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
      path('contact' , views.contact, name ='contact'),
      path('about' , views.about, name ='about'),
      path('search' , views.search , name = "search"),
-     
-]
+     path('signup' , views.handleSignUp  , name = "handleSignUp"), 
+     path('login', views.handelLogin ,   name =  "handelLogin"),     
+     path('logout', views.handelLogout ,  name =  "handelLogout"),
+ ]
