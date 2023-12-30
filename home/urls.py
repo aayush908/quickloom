6e969bfabcffc75d3061a569f0096f83e221d09a
Aayush kapil <aayushkapil100@gmail.com>
2023-12-30 19:24:30 +0530

    requirement.txt

--- home/urls.py
+++ home/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('contact', views.contact, name="contact"),
     path('about', views.about, name="about"),
     path('search', views.search, name="search"),
-    path('search', views.search, name="search"),
+    
     path('signup', views.handleSignUp, name="handleSignUp"),
     path('login', views.handelLogin, name="handleLogin"),
     path('logout', views.handelLogout, name="handleLogout"),
